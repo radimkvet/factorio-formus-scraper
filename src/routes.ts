3756abeac1ad309b9b993d1fbb5e0ede7e470cff
Radim Kvet <radim.kvet@apify.com>
2026-02-16 14:03:18 +0100

    feat: create handler for forum index page

--- src/routes.ts
+++ src/routes.ts
@@ -1,2 +1,5 @@
 import { createCheerioRouter } from 'crawlee';
+import { forumSearchStartHandler } from './routes/searchStart.js';
+import { Label } from './types/label.js';
 export const router = createCheerioRouter();
+router.addHandler(Label.FORUM_SEARCH_START, forumSearchStartHandler);
